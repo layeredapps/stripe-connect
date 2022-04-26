4cbc078918d5d7334633e55d9d462912821e5971
beninsydney <benodev@protonmail.com>
2022-04-26 20:40:23 +1000

    Filter by appid

--- src/www/api/administrator/connect/payouts-count.js
+++ src/www/api/administrator/connect/payouts-count.js
@@ -3,22 +3,16 @@ const connect = require('../../../../../index.js')
 module.exports = {
   get: async (req) => {
     req.query = req.query || {}
-    let where
-    if (req.query.stripeid) {
-      where = {
-        where: {
-          stripeid: req.query.stripeid,
-          appid: req.appid || global.appid
-        }
-      }
-    } else if (req.query.accountid) {
-      where = {
-        where: {
-          accountid: req.query.accountid,
-          appid: req.appid || global.appid
-        }
-      }
+    const where = {
+      appid: req.appid || global.appid
     }
-    return connect.Storage.Payout.count(where)
+    if (req.query.stripeid) {
+      where.stripeid = req.query.stripeid
+    } else if (req.query.accountid) {
+      where.accountid = req.query.accountid
+    }
+    return connect.Storage.Payout.count({
+      where
+    })
   }
 }
