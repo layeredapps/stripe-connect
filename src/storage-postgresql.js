3cb9bf81e71643255a9166358de2f7025a60239d
beninsydney <benodev@protonmail.com>
2022-05-02 18:26:35 +1000

    fix: postgres ssl

--- src/storage-postgresql.js
+++ src/storage-postgresql.js
@@ -2,8 +2,27 @@ const { Sequelize } = require('sequelize')
 const Log = require('@layeredapps/dashboard/src/log.js')('sequelize-stripe-connect-postgresql')
 
 module.exports = async () => {
-  const prefixedDatabaseURL = process.env.CONNECT_POSTGRESQL_DATABASE_URL || process.env.POSTGRESQL_DATABASE_URL
-  const sequelize = new Sequelize(prefixedDatabaseURL, {
+  let url = process.env.CONNECT_POSTGRESQL_DATABASE_URL || process.env.POSTGRESQL_DATABASE_URL
+  const sslModeRequiredIndex = url.indexOf('?sslmode=require')
+  let dialectOptions
+  if (sslModeRequiredIndex > -1) {
+    url = url.substring(0, sslModeRequiredIndex)
+    dialectOptions = {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      },
+      keepAlive: true
+    }
+  }
+  const sequelize = new Sequelize(url, {
+    dialect: 'postgres',
+    dialectOptions,
+    pool: {
+      max: process.env.CONNECT_MAX_CONNECTIONS || process.env.MAX_CONNECTIONS || 10,
+      min: 0,
+      idle: process.env.CONNECT_IDLE_CONNECTION_LIMIT || process.env.IDLE_CONNECTION_LIMIT || 10000
+    },
     logging: (sql) => {
       return Log.info(sql)
     }
